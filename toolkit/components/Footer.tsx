529bbf802f3478904c0ab5d93edbce5a7f28d93d
christianbueschi <christianbueschi@gmail.com>
2023-02-02 16:07:19 +0100

    add gh link

--- toolkit/components/Footer.tsx
+++ toolkit/components/Footer.tsx
@@ -2,6 +2,7 @@ import { Stack, Text, useColorModeValue, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { ShareLink } from './ShareLink';
+import { FiGithub } from 'react-icons/fi';
 
 export const Footer: React.FC = () => {
   const { t } = useTranslation(['common']);
@@ -16,12 +17,19 @@ export const Footer: React.FC = () => {
       justifyContent='space-between'
     >
       <ShareLink />
-      <Stack alignItems='end' flexDirection='row' gap={8}>
+      <Stack alignItems='center' flexDirection='row' gap={8} spacing={0}>
         <Link href='/privacy' passHref>
           <Text>{t('privacyPolicyLink')}</Text>
         </Link>
         <Link href='mailto:hello@pooma.app' passHref>
           <Text>{t('email')}</Text>
+        </Link>
+        <Link
+          href='https://github.com/christianbueschi/pooma'
+          passHref
+          target='_blank'
+        >
+          <FiGithub />
         </Link>
       </Stack>
     </VStack>
